bc1ccf9775ac8b97d8d6ff99b3b40c35ab084732
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-11-23 12:15:32 +0100

    feat: Add Cookie Consent Bottom Sheet component

---
+++ src/app/components/cookie-consent-bottom-sheet/cookie-consent-bottom-sheet.ts
@@ -0,0 +1,58 @@
+import {DialogRef} from '@angular/cdk/dialog';
+import {Component, ElementRef, inject, OnInit, ViewEncapsulation} from '@angular/core';
+import {Cookies} from '../../services/cookies';
+import {Button} from '../../ui/button/button';
+
+@Component({
+  selector: 'app-cookie-consent-bottom-sheet',
+  imports: [
+    Button
+  ],
+  templateUrl: './cookie-consent-bottom-sheet.html',
+  styleUrl: './cookie-consent-bottom-sheet.scss',
+  encapsulation: ViewEncapsulation.None,
+  host : {
+    'class': 'cookie-consent-bottom-sheet__wrapper',
+    'animate.enter': 'cookie-consent-bottom-sheet__enter'
+  }
+})
+export class CookieConsentBottomSheet implements OnInit {
+
+  private readonly _cookies = inject(Cookies);
+  private readonly _dialogRef = inject(DialogRef<CookieConsentBottomSheet>);
+  private readonly _elementRef = inject(ElementRef<HTMLElement>);
+
+  ngOnInit() {
+    this._elementRef.nativeElement.addEventListener('animationend', (event: AnimationEvent) => {
+      if (event.animationName.endsWith('__leave')) {
+        this._dialogRef.close();
+      }
+    });
+  }
+
+  rejectAllCookies() {
+
+    this._cookies.rejectAll();
+
+    this._cookies.set('cookieConsent', 'true', {
+      expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000)
+    });
+
+    this.close();
+  }
+
+  acceptAllCookies() {
+
+    this._cookies.acceptAll();
+
+    this._cookies.set('cookieConsent', 'true', {
+      expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000)
+    });
+
+    this.close();
+  }
+
+  close() {
+    this._elementRef.nativeElement.classList.add('cookie-consent-bottom-sheet__leave');
+  }
+}
