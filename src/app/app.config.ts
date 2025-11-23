9a22923ebfe0f2c0b2f3faaef69eaaf4e8df64b9
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-11-23 14:09:35 +0100

    refactor: Inline Firebase configuration into `app.config.ts` and remove `firebase.config.ts`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {isPlatformServer} from '@angular/common';
+import {isPlatformBrowser, isPlatformServer} from '@angular/common';
 import {
   ApplicationConfig,
   CSP_NONCE,
@@ -9,30 +9,23 @@ import {
   REQUEST_CONTEXT
 } from '@angular/core';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {getAnalytics} from 'firebase/analytics';
+import {initializeApp} from 'firebase/app';
+import {initializeAppCheck, ReCaptchaEnterpriseProvider} from 'firebase/app-check';
+import {connectAuthEmulator, getAuth} from 'firebase/auth';
+import {connectFirestoreEmulator, getFirestore} from 'firebase/firestore';
+import {connectFunctionsEmulator, getFunctions} from 'firebase/functions';
+import {connectStorageEmulator, getStorage} from 'firebase/storage';
+import {environment} from '../environments/environment';
 import {CookieConsentBottomSheet} from './components/cookie-consent-bottom-sheet/cookie-consent-bottom-sheet';
-import {provideFirebase} from './config/firebase.config';
 import {BottomSheet} from './services/bottom-sheet';
 import {Cookies} from './services/cookies';
+import {Analytics, AppCheck, Auth, FirebaseApp, FirebaseStorage, Firestore, Functions} from './tokens/firebase.tokens';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideClientHydration(withEventReplay()),
-    provideFirebase(),
-    {
-      provide: CSP_NONCE,
-      useFactory: () => {
-
-        const platformId = inject(PLATFORM_ID);
-        const reqContext = inject(REQUEST_CONTEXT) as {nonce: string};
-
-        if (isPlatformServer(platformId) && reqContext) {
-          return reqContext.nonce;
-        }
-
-        return null;
-      }
-    },
     provideAppInitializer(() => {
 
       const platformId = inject(PLATFORM_ID);
@@ -48,6 +41,123 @@ export const appConfig: ApplicationConfig = {
         cookies.rejectAll();
         bottomSheet.open(CookieConsentBottomSheet);
       }
-    })
+    }),
+    provideAppInitializer(() => {
+
+      const platformId = inject(PLATFORM_ID);
+
+      if (!environment.production && isPlatformBrowser(platformId)) {
+        // @ts-expect-error
+        window.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+      }
+    }),
+    {
+      provide: FirebaseApp,
+      useValue: initializeApp(environment.firebase)
+    },
+    {
+      provide: Auth,
+      useFactory: () => {
+
+        const platformId = inject(PLATFORM_ID);
+
+        const auth = getAuth();
+
+        if (!environment.production && isPlatformBrowser(platformId)) {
+          connectAuthEmulator(auth, `${environment.emulators.auth.protocol}://${environment.emulators.auth.host}:${environment.emulators.auth.port}`);
+        }
+
+        return auth;
+      }
+    },
+    {
+      provide: AppCheck,
+      useFactory: () => {
+
+        const provider = new ReCaptchaEnterpriseProvider(environment.recaptchaEnterprise);
+
+        const appCheck = initializeAppCheck(undefined, {
+          provider,
+          isTokenAutoRefreshEnabled: true
+        });
+
+        return appCheck;
+      }
+    },
+    {
+      provide: Firestore,
+      useFactory: () => {
+
+        const platformId = inject(PLATFORM_ID);
+
+        const firestore = getFirestore();
+
+        if (!environment.production && isPlatformBrowser(platformId)) {
+          connectFirestoreEmulator(firestore, environment.emulators.firestore.host, environment.emulators.firestore.port);
+        }
+      }
+    },
+    {
+      provide: Functions,
+      useFactory: () => {
+
+        const platformId = inject(PLATFORM_ID);
+        const firebaseApp = inject(FirebaseApp);
+
+        const functions = getFunctions(firebaseApp, 'europe-central2');
+
+        if (!environment.production && isPlatformBrowser(platformId)) {
+          connectFunctionsEmulator(functions, environment.emulators.functions.host, environment.emulators.functions.port);
+        }
+
+        return functions;
+      }
+    },
+    {
+      provide: FirebaseStorage,
+      useFactory: () => {
+
+        const platformId = inject(PLATFORM_ID);
+
+        const storage = getStorage();
+
+        if (!environment.production && isPlatformBrowser(platformId)) {
+          connectStorageEmulator(storage, environment.emulators.storage.host, environment.emulators.storage.port);
+        }
+
+        return storage;
+      }
+    },
+    {
+      provide: Analytics,
+      useFactory: () => {
+
+        const platformId = inject(PLATFORM_ID);
+        const cookies = inject(Cookies);
+
+        if (isPlatformBrowser(platformId)) {
+
+          cookies.rejectAll();
+
+          return getAnalytics();
+        }
+
+        return null;
+      }
+    },
+    {
+      provide: CSP_NONCE,
+      useFactory: () => {
+
+        const platformId = inject(PLATFORM_ID);
+        const reqContext = inject(REQUEST_CONTEXT) as {nonce: string};
+
+        if (isPlatformServer(platformId) && reqContext) {
+          return reqContext.nonce;
+        }
+
+        return null;
+      }
+    }
   ]
 };
