61869be1f59bf0ca29a3b5c19baca112f97368ab
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-11-23 14:10:14 +0100

    feat: Track page views with Firebase Analytics in `App` component using `ngOnInit`

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Footer} from './components/footer/footer';
 import {Hero} from './components/hero/hero';
+import {logEvent} from 'firebase/analytics';
+import {Analytics} from './tokens/firebase.tokens';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +13,15 @@ import {Hero} from './components/hero/hero';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+
+  private readonly _analytics = inject(Analytics, {optional: true});
+
+  ngOnInit() {
+    if (this._analytics) {
+      logEvent(this._analytics, 'page_view', {
+        method: 'ngOnInit'
+      });
+    }
+  }
 }
