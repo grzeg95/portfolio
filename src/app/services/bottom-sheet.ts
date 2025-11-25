49977192d91b0e69bcbc6b28e244c24cb03c11fe
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-11-25 21:31:30 +0100

    feat: Add block scroll support to Bottom Sheet configuration

--- src/app/services/bottom-sheet.ts
+++ src/app/services/bottom-sheet.ts
@@ -1,5 +1,5 @@
 import {Dialog, DialogConfig, DialogRef} from '@angular/cdk/dialog';
-import {createGlobalPositionStrategy} from '@angular/cdk/overlay';
+import {createGlobalPositionStrategy, Overlay} from '@angular/cdk/overlay';
 import {BasePortalOutlet, ComponentType} from '@angular/cdk/portal';
 import {inject, Injectable, Injector} from '@angular/core';
 
@@ -10,9 +10,14 @@ export class BottomSheet {
 
   private readonly _injector = inject(Injector);
   private readonly _dialog = inject(Dialog);
+  private readonly _overlay = inject(Overlay);
   private _dialogRef?: DialogRef<any, any>;
 
-  open<T>(component: ComponentType<T>, config?: DialogConfig<unknown, DialogRef<any, T>, BasePortalOutlet> | undefined) {
+  open<T>(
+    component: ComponentType<T>,
+    config?: DialogConfig<unknown, DialogRef<any, T>, BasePortalOutlet> | undefined,
+    blockScroll?: boolean
+  ) {
 
     if (typeof this._dialogRef?.componentRef?.instance.close === 'function') {
       this._dialogRef.componentRef.instance.close();
@@ -29,6 +34,7 @@ export class BottomSheet {
         autoFocus: '__non_existing_element__',
         restoreFocus: false,
         hasBackdrop: false,
+        scrollStrategy: blockScroll ? this._overlay.scrollStrategies.block() : this._overlay.scrollStrategies.noop(),
         ...config
       }
     );
