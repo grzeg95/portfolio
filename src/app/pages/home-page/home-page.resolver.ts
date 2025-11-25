5de64f165e767cfb69ef60c72ae93fbb060d0318
Grzegorz Eleryk <dev.grzegorz.eleryk@gmail.com>
2025-11-25 15:53:47 +0100

    feat: Add HomePage resolver and update HomePage component to display skills data

---
+++ src/app/pages/home-page/home-page.resolver.ts
@@ -0,0 +1,12 @@
+import {inject} from '@angular/core';
+import {getDoc} from 'firebase/firestore';
+import {getHomePageRef} from '../../models/home-page.firestore';
+import {Firestore} from '../../tokens/firebase.tokens';
+
+export const homePageResolver = () => {
+
+  const firestore = inject(Firestore);
+
+  const homePageRef = getHomePageRef(firestore);
+  return getDoc(homePageRef).then(async (doc) => doc.data());
+};
--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,15 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {ActivatedRoute} from '@angular/router';
 import {Footer} from '../../components/footer/footer';
 import {Hero} from '../../components/hero/hero';
+import {Skills} from '../../components/skills/skills';
+import {HomePageFirestore} from '../../models/home-page.firestore';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     Footer,
-    Hero
+    Hero,
+    Skills
   ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
 export class HomePage {
+  private readonly _activatedRoute = inject(ActivatedRoute);
+  private readonly data = toSignal(this._activatedRoute.data);
+  protected _homePage = computed(() => this.data()?.['homePage'] as HomePageFirestore);
 }
